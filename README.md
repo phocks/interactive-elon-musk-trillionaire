04637739f687607e49c887780913f61bec1cfcab
Joshua Byrd <phocks@gmail.com>
2026-06-10 11:38:12 +1000

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# interactive-elon-musk-trillionaire
+
+A project generated from [aunty](https://github.com/abcnews/aunty)'s `svelte` project template.
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import acto from '@abcnews/alternating-case-to-object';
+import { whenOdysseyLoaded } from '@abcnews/env-utils';
+import { getMountValue, selectMounts } from '@abcnews/mount-utils';
+import type { Mount } from '@abcnews/mount-utils';
+import App from './components/App/App.svelte';
+import { mount } from 'svelte';
+
+let appMountEl: Mount;
+let appProps;
+
+whenOdysseyLoaded.then(() => {
+  [appMountEl] = selectMounts('interactiveelonmusktrillionaire');
+
+  if (appMountEl) {
+    appProps = acto(getMountValue(appMountEl));
+    
+    mount(App, {
+      target: appMountEl,
+      props: appProps
+    });
+  }
+});
+
+if (process.env.NODE_ENV === 'development') {
+  console.debug(`[interactive-elon-musk-trillionaire] public path: ${__webpack_public_path__}`);
+}
